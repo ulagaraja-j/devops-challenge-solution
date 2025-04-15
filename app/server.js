6d41f3269bbb9fb99e17e4228932a46bad89f8fc
ulagaraja-j <world.j.github@gmail.com>
2025-04-15 18:50:03 +0530

    simpletimeservice nodejs server added along with Dockerfile

---
+++ app/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.set('trust proxy', true);
+
+app.get('/', (req, res) => {
+    const timestamp = new Date().toISOString(); // Shows the UTC time by default
+    const ip = req.ip;
+    console.log(req);
+    res.json({
+        timestamp,
+        ip
+    });
+});
+
+app.listen(port, '0.0.0.0', () => {
+    console.log('Server running on port 5000');
+});
+
